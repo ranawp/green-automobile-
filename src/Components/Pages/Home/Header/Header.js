80ae939e4d19185d305f7a457c5305d5ee59e0a4
ranawp <ranasaha4224#gmail.com>
2022-05-22 21:18:11 +0600

    banner added

--- src/Components/Pages/Home/Header/Header.js
+++ src/Components/Pages/Home/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import banner1 from '../../../../assets/images/banner1.jpg'
+import banner2 from '../../../../assets/images/banner2.jpg'
+import banner3 from '../../../../assets/images/banner3.jpg'
+import './Header.css'
 const Header = () => {
+
     return (
-        <div>
-            <h1>This is header</h1>
+        <div class="hero min-h-screen banner" >
+            <div class="hero-overlay bg-opacity-50"></div>
+            <div class="hero-content text-center text-neutral-content">
+                <div class="max-w-md">
+                    <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+                    <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+            </div>
         </div>
     );
 };
