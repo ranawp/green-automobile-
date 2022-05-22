10f42c24e8fcee34dd34e179ba03da40528c8ad0
ranawp <ranasaha4224#gmail.com>
2022-05-22 13:09:38 +0600

    navbar added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Shared/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route>
+
+        </Route>
+      </Routes>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0077A3",
+          secondary: "#FEB606",
+          accent: "#1DB99A",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+
+
+}
+
+// 0077A3
