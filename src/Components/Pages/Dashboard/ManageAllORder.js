964271df4c271326fc4d41e2aad6da0de58b5d4e
ranawp <ranasaha4224#gmail.com>
2022-05-25 08:30:13 +0600

    manage order page added

---
+++ src/Components/Pages/Dashboard/ManageAllORder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageAllORder = () => {
+    return (
+        <div>
+            <h3>Manage all orders. Its maintain only admin</h3>
+        </div>
+    );
+};
+
+export default ManageAllORder;
