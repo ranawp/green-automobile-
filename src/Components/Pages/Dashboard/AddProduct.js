204de31778523885444cc234a55337b5c2370e85
ranawp <ranasaha4224#gmail.com>
2022-05-25 08:30:59 +0600

    add new product page added

---
+++ src/Components/Pages/Dashboard/AddProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddProduct = () => {
+    return (
+        <div>
+            <h3>Add New Product</h3>
+
+        </div>
+    );
+};
+
+export default AddProduct;
