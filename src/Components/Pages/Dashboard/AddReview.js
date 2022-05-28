b59345ea62e9244cb1635f25d84e03c9559aba94
ranawp <ranasaha4224#gmail.com>
2022-05-28 20:19:45 +0600

    review page added

--- src/Components/Pages/Dashboard/AddReview.js
+++ src/Components/Pages/Dashboard/AddReview.js
@@ -46,77 +46,6 @@ const AddReview = () => {
     }
 
 
-    // const handleSubmit = event => {
-    //     event.preventDefault();
-    //     const clientName = event.target.name.value;
-    //     const clientImg = event.target.image.value;
-    //     const rattings = event.target.rattings.value;
-    //     const massage = event.target.massage.value;
-    //     /////////////
-    //     console.log(event);
-    //     // const image = event.image[0];
-    //     // const formData = new FormData();
-    //     // formData.append('image', image);
-    //     // const url = `https://api.imgbb.com/1/upload?key=${imageStoragekey}`;
-    //     // fetch(url, {
-    //     //     method: 'POST',
-    //     //     body: formData
-    //     // })
-    //     //     .then(res => res.json())
-    //     //     .then(result => {
-    //     //         console.log(result)
-    //     //         if (result.success) {
-    //     //             const img = result.data.url;
-    //     //             const reviews = {
-    //     //                 clientName: clientName,
-    //     //                 clientImg: clientImg,
-    //     //                 rattings: rattings,
-    //     //                 massage: massage,
-    //     //                 img: img
-    //     //             }
-    //     //             fetch('https://calm-coast-29564.herokuapp.com/reviews', {
-    //     //                 method: 'POST',
-    //     //                 headers: {
-    //     //                     'content-type': 'application/json'
-    //     //                 },
-    //     //                 body: JSON.stringify(reviews)
-    //     //             })
-    //     //                 .then(res => res.json())
-    //     //                 .then(result => {
-    //     //                     console.log('reviews', result)
-    //     //                 })
-    //     //         }
-    //     //     })
-
-
-    // }
-
-    // const handleSubmit = event => {
-    //     event.preventDefault();
-    //     const clientName = event.target.name.value;
-    //     const clientImg = event.target.image.value;
-    //     const rattings = event.target.rattings.value;
-    //     const massage = event.target.massage.value;
-
-    //     const reviews = {
-    //         clientName: clientName,
-    //         clientImg: clientImg,
-    //         rattings: rattings,
-    //         massage: massage
-    //     }
-    //     fetch('https://calm-coast-29564.herokuapp.com/reviews', {
-    //         method: 'POST',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(reviews)
-    //     })
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data)
-    //         })
-    // }
-
 
     return (
         <div className='mt-5 p-6 w-full'>
@@ -136,20 +65,9 @@ const AddReview = () => {
 
                     <small>Image:</small> <br /><input class="input input-bordered w-50 max-w-xs" placeholder='Quantity' type="file" {...register("image")} /> <br />
 
-                    <input type="submit" value='Add Product' className='mt-2 text-white btn btn-active btn-primary  max-w-xs' />
+                    <input type="submit" value='Add Review' className='mt-2 text-white btn btn-active btn-primary  max-w-xs' />
                 </form>
-                {/* <form action="" onSubmit={handleSubmit} >
-                    <small>Name:</small><br /><input name='name' type="text" placeholder="Your Name" class="input input-bordered w-full max-w-xs" /> <br />
 
-
-                 
-
-                    <small>Rattings out of five.Example:3/5 </small><br /><input name='rattings' type="text" placeholder="Rattings" class="input input-bordered w-full max-w-xs" /> <br />
-
-                    <small>Write Massage:</small> <br /><textarea name='massage' class="textarea textarea-bordered max-w-xs" placeholder="Write your massage..."></textarea> <br />
-                    <small>Image:</small><br /><input type="file" name='image' placeholder="Your Image" class="input input-bordered w-full max-w-xs" /> <br />
-                    <input type="submit" value='SUBMIT' className='text-white btn btn-active btn-primary  max-w-xs' /> <br />
-                </form> */}
 
 
 
